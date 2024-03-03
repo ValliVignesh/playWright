a3ea369451825e15f1d65705960f430d482d2e6e
Valliammai N <valliammai.usha@gmail.com>
2024-03-03 14:06:07 +0530

    Locator files

---
+++ classwork.spec.ts
@@ -0,0 +1,27 @@
+    //Open the ur("http://leaftaps.com/opentaps/control/main");
+    
+    //Login using the username and password
+  
+    //Click CRM/SFA
+
+    //Click Leads
+
+    //Click Create Leads
+   
+    //Enter the company name
+
+    //Enter the first name
+   
+    //Enter the last name
+    
+    //Select the dropdown value from Source using index
+    //Select the dropdown value from Industry using value and get the count
+    //Select the dropdown value from Ownership using label
+
+   //Select the dropdown value from the preferred currency
+
+    //Select the dropdown value from State
+    
+    //Click Submit
+ 
+    //Verify the status
